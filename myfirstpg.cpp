dc8566fdfaaff4d2312c55ab16bc461f158014b8
paul <duddnrdpqj@gmail.com>
2025-04-09 19:34:03 +0900

    start

---
+++ myfirstpg.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+// 단일  주석
+/*
+다중 주석
+*/
+int main()
+{
+    cout << "Hello World! \n";
+    cout << "I am learning C++" << "\n\n";
+    cout << 3 << endl;
+    return 0;
+}
---
+++ var.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int myNum = 28;
+    cout << myNum << "\n";
+    double myFloatNum = 5.99;
+    cout << myFloatNum << endl;
+    char myLetter = 'K';
+    cout << myLetter << endl;
+    string myText = "Hello";
+    cout << myText << endl;
+    bool myBoolean = false;
+    cout << myBoolean << endl;
+    cout << "I am" << myNum << " years old.";
+
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+    cout << sum;
+    cout << x + y;
+
+    return 0;
+}
