70c6ed68c521e23e12ea54c076c74dfbd76d8df0
paul <duddnrdpqj@gmail.com>
2025-04-10 17:30:13 +0900

    day2

--- var.cpp
+++ var.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 int main()
 {
-    int myNum = 28;
+    int myNum = 28; // 2 or 4bytes
     cout << myNum << "\n";
-    double myFloatNum = 5.99;
+    double myFloatNum = 5.99; // 8bytes floas = 4bytes
     cout << myFloatNum << endl;
-    char myLetter = 'K';
+    char myLetter = 'K'; // 1byte
     cout << myLetter << endl;
     string myText = "Hello";
     cout << myText << endl;
-    bool myBoolean = false;
-    cout << myBoolean << endl;
+    bool myBoolean = false;    // 1byte
+    cout << myBoolean << endl; // print 0 // true = 1
     cout << "I am" << myNum << " years old.";
 
     int x = 5;
@@ -20,6 +21,9 @@ int main()
     int sum = x + y;
     cout << sum;
     cout << x + y;
+
+    char a = 65;
+    cout << a; // output A
 
     return 0;
 }
